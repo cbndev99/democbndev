a43a7aedb5c2ac9b5dd20a6510925604fb2234df
cbndev99 <cbndev19@gmail.com>
2024-12-26 23:05:19 +0800

    added image

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,22 +1,28 @@
 import PageHeader from "../components/PageHeader";
-import { Binoculars, Goal } from "lucide-react";
+import { Eye, Goal } from "lucide-react";
+import P1 from "../assets/img/p1.jpg";
 
 function AboutUsPage() {
   return (
     <div className="w-full flex_center flex-col mt-20">
-      <div className="content_wrapper">
-        <PageHeader title="About Us" />
-        <p className="text-pretty mt-8">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industrys standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
+      <div className="content_wrapper flex flex-col-reverse md:flex-row gap-10 justify-center items-center px-8">
+        <div>
+          <PageHeader title="Our Story" />
+          <p className="text-justify mt-8">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industrys standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+        <div>
+          <img src={P1} alt="aboutus-photo" loading="lazy" />
+        </div>
       </div>
 
       <div className="mt-20 bg-white px-8 py-16 w-full flex justify-center">
@@ -27,13 +33,13 @@ function AboutUsPage() {
           <div className="grid grid-cols-1 md:grid-cols-2 items-start gap-[3rem] md:gap-[5rem]">
             <div className="flex justify-start items-center gap-8 mt-10">
               <div className="bg-slate-100 p-3 flex items-center justify-center rounded-lg shadow-md">
-                <Binoculars size={35} />
+                <Eye size={35} />
               </div>
               <div>
                 <h2 className="font-bold text-lg text-orange-500">
                   Our Vision
                 </h2>
-                <p className="mt-3">
+                <p className="mt-3 text-justify">
                   To be the most customer-centric company, offering products and
                   services that bring convenience and joy to everyday life.
                 </p>
@@ -47,7 +53,7 @@ function AboutUsPage() {
                 <h2 className="font-bold text-lg text-orange-500">
                   Our Mission
                 </h2>
-                <p className="mt-3">
+                <p className="mt-3 text-justify">
                   To offer a diverse range of high-quality products at
                   competitive prices, ensuring an exceptional shopping
                   experience for every customer.
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,23 @@
+import P2 from "../assets/img/p2.jpg";
+
 function HomePage() {
-  return <div className="h-svh">HomePage</div>;
+  return (
+    <div className="flex justify-center items-center gap-10">
+      <div className="">
+        <h1 className="text-6xl font-extrabold">
+          Redefining Modern Spaces <br></br> with Exceptional Craftsmanship
+        </h1>
+        <h2 className="mt-8">
+          Custom-made solutions tailored to your needs—whether it’s gates,
+          balcony fencing, roofing, and many more.
+        </h2>
+        <button>Contact Us</button>
+      </div>
+      <div>
+        <img src={P2} alt="hero-img" />
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
